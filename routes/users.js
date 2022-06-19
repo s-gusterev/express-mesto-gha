4df0a6c3e768983ec658c2f95750fb0e3098fc92
s-gusterev <copywriter-sg@yandex.ru>
2022-06-19 17:21:39 +0300

    metod patch

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const usersrouter = require('express').Router();
-const { createUser, getUser, getUserId } = require('../controllers/users');
+const { createUser, getUser, getUserId, patchUserProfile } = require('../controllers/users');
 
 usersrouter.post('/', createUser);
 
 usersrouter.get('/', getUser);
 
 usersrouter.get('/:id', getUserId);
+usersrouter.patch('/me', patchUserProfile);
 
 module.exports = usersrouter;
