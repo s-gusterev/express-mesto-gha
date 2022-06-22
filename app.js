9736e55fab33a4841ddd2ad205166d085cb0574d
s-gusterev <copywriter-sg@yandex.ru>
2022-06-22 11:55:15 +0300

    исправление после ревью 1

--- app.js
+++ app.js
@@ -27,9 +27,6 @@ app.use((req, res) => {
 });
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  // useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => { console.log(`Сервер работает на порту ${PORT}`); });
--- models/card.js
+++ models/card.js
@@ -16,7 +16,7 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, default: [] }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user', default: [] }],
   createdAt: {
     type: Date,
     default: Date.now,
