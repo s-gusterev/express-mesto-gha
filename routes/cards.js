0ef2ec0efe479c6527e540d6fb21061befa2aed6
s-gusterev <copywriter-sg@yandex.ru>
2022-07-04 21:13:39 +0300

    исправления после ревью

--- routes/cards.js
+++ routes/cards.js
@@ -7,7 +7,7 @@ const {
 cardsrouter.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regex(/(http|https):\/\/([\w.]+\/?)\S*/),
+    link: Joi.string().required().regex(/^((http|https):\/\/)?(www\.)?([A-Za-zА0-9]{1}[A-Za-zА0-9-]*\.?)*\.{1}[A-Za-zА0-9-]{2,}(\/([\w#!:.?+=&%@!\-/])*)?/),
   }),
 }), createCard);
 
@@ -15,24 +15,24 @@ cardsrouter.get('/', getCard);
 
 cardsrouter.get('/:id', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
+    id: Joi.string().hex().length(24),
   }),
 }), getCardId);
 
 cardsrouter.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }),
 }), deleteCard);
 
 cardsrouter.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }),
 }), likeCard);
 cardsrouter.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }),
 }), dislikeCard);
 
